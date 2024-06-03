3bada10d19fe4a1c43b2dc25cf3872f1665836d7
Tristan <sen.hou2023@gmail.com>
2024-06-03 21:38:25 +0000

    add void pointer to refactor code

--- lecture2/swap.c
+++ lecture2/swap.c
@@ -4,7 +4,7 @@
 
 typedef struct Person
 {
-
+// how memory store struct? when use point of Person change value what got swap
 	char firstName[20];
 	char lastName[20];
 } Person;
@@ -28,6 +28,37 @@ void swapPersons(Person* px, Person* py){
 	*py = tmp;
 }
 
+// code duplication for swap
+//
+//
+// using void pointer; means pointer for any datatype!
+//
+//
+
+//refactor using void pointer; need to know the size of that type;
+// size_t is a alias of unsigned int;
+
+
+void swapAny(void* px, void* py, size_t numBytes)
+{
+	void* tmp;
+
+	tmp = malloc(numBytes);
+	if(tmp == NULL){
+		printf("Failed to allocate memory.\n");
+		exit(1);
+	}
+
+	memcpy(tmp, px, numBytes);
+
+	memcpy(px, py, numBytes);
+	memcpy(py, tmp, numBytes);
+	
+	// prevent memory leak;
+	free(tmp);
+
+}
+
 int main(int argc, char* argv[]){
 	if(argc != 3 ||
 		strcmp(argv[1], "-h") == 0 ||
@@ -37,15 +68,26 @@ int main(int argc, char* argv[]){
 	}
 	int x = atoi(argv[1]);
 	int y = atoi(argv[2]);
-
+	/*
 	printf("Before swap: %d %d\n", x, y);
 	swapIntegers(&x, &y);
 	printf("After swap: %d %d\n", x, y);
-	
+	*/	
 	Person p1 = {"Jack", "Ali"};
 	Person p2 = {"Tom", "H"};
+	/*
 	printf("Before swap p1:%s,%s, p2: %s,%s\n", p1.firstName, p1.lastName, p2.firstName, p2.lastName);	
  	swapPersons(&p1, &p2);	
 	printf("After swap p1:%s,%s, p2: %s,%s\n", p1.firstName, p1.lastName, p2.firstName, p2.lastName);	
+	*/
+	
+	printf("Before swap: %d %d\n", x, y);
+	swapAny(&x, &y, sizeof(int));
+	printf("Before swap: %d %d\n", x, y);
+	
+	
+	printf("Before swap p1:%s,%s, p2: %s,%s\n", p1.firstName, p1.lastName, p2.firstName, p2.lastName);	
+ 	swapAny(&p1, &p2, sizeof(Person));	
+	printf("After swap p1:%s,%s, p2: %s,%s\n", p1.firstName, p1.lastName, p2.firstName, p2.lastName);		
 	exit(0);
 }
